386ed6e7e2eac5e12af129d4dad49039b8bcbef4
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-06-19 19:13:21 -0300

    Criação das classes dos objetos e da interface

---
+++ src/main/java/Hospital.java
@@ -0,0 +1,22 @@
+public class Hospital implements Setor {
+
+    private static Hospital instancia = new Hospital();
+
+    private Hospital() {}
+
+    public static Hospital getInstancia() {
+        return instancia;
+    }
+
+    public String receberReclamacao(String mensagem) {
+        return "O Hospital vai procurar melhorar o serviço da reclamação: " + mensagem;
+    }
+
+    public String receberElogio(String mensagem) {
+        return "O Hospital agradece o feedback: " + mensagem;
+    }
+
+    public String receberSugestao(String mensagem) {
+        return "O Hospital vai analisar a sua sugestão: " + mensagem;
+    }
+}
